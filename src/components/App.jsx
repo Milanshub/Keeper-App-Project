01f85a16bb41f379f6c928e8d5b0b4b12d452cb6
Milan <m26030820@gmail.com>
2024-01-22 14:49:20 +0100

    finished version - useState, component tree, spread

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,55 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
+import { useState } from "react";
+
 
 function App() {
+  const[notes, setNote] = useState([]);
+
+
+  function addNote(newNote){
+    setNote(prevNotes => {
+     return [...prevNotes, newNote];
+    });
+  } 
+
+  function deleteNote(id){
+    setNote(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !==id; 
+      })
+    })
+  }
+
   return (
     <div>
       <Header />
-      {notes.map((noteItem) => (
-        <Note
-          key={noteItem.key}
+      <CreateArea 
+        onAdd={addNote}
+      />
+    {  notes.map((noteItem, index) => {
+        return <Note 
+          key={index}
+          id={index}
           title={noteItem.title}
           content={noteItem.content}
-        />
-      ))}
+          onDelete={deleteNote}
+        /> 
+      })}
+      <Note key={1} title="Note Title" content="Note Content" />
       <Footer />
     </div>
   );
 }
+
+//2. Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
+
+//This is the end result you're aiming for:
+//https://pogqj.csb.app/
 
 export default App;
